5f73d0a58335ff378dd13c99d498add3396ccd2c
ildansaydiev <isaydiev86@gmail.com>
2023-04-23 19:30:01 +0500

    HW3 fix (delete slice struct)

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -20,27 +20,15 @@ func Top10(s string) []string {
 }
 
 func sortWords(count int, words map[string]int) []string {
-	type keyValue struct {
-		Key   string
-		Value int
-	}
 	wordsLen := len(words)
-	sortedStruct := make([]keyValue, 0, wordsLen)
-
-	for key, value := range words {
-		sortedStruct = append(sortedStruct, keyValue{key, value})
+	temp := make([]string, 0, wordsLen)
+	for key := range words {
+		temp = append(temp, key)
 	}
-
-	sort.Slice(sortedStruct, func(i, j int) bool {
-		return (sortedStruct[i].Value > sortedStruct[j].Value) ||
-			(sortedStruct[i].Value == sortedStruct[j].Value && sortedStruct[i].Key < sortedStruct[j].Key)
+	sort.Slice(temp, func(i, j int) bool {
+		return (words[temp[i]] > words[temp[j]]) ||
+			(words[temp[i]] == words[temp[j]] && temp[i] < temp[j])
 	})
-
-	result := make([]string, 0, count)
-
-	for i := 0; (i < count) && (i < wordsLen); i++ {
-		result = append(result, sortedStruct[i].Key)
-	}
-
-	return result
+	temp = temp[0:count]
+	return temp
 }
