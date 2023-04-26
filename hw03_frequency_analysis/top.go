a5718a874331ff7b2fcd55fb17d6613ff350dfff
isaydiev86 <isaydiev86@gmail.com>
2023-04-26 09:25:55 +0500

    HW3 is completed (#3)
    * HW3 is completed
    
    * HW3 fix (delete slice struct)
    
    * HW3 fix (add test with one word)
    
    * HW3 fix (wordsLen > count)
    
    * HW3 fix (delete else)
    
    * HW3 fix ([0:])

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,36 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(s string) []string {
+	if len(s) == 0 {
+		return nil
+	}
+	words := make(map[string]int)
+	strSlice := strings.Fields(s)
+	for _, word := range strSlice {
+		if len(word) > 0 {
+			words[word]++
+		}
+	}
+	return sortWords(10, words)
+}
+
+func sortWords(count int, words map[string]int) []string {
+	wordsLen := len(words)
+	temp := make([]string, 0, wordsLen)
+	for key := range words {
+		temp = append(temp, key)
+	}
+	sort.Slice(temp, func(i, j int) bool {
+		return (words[temp[i]] > words[temp[j]]) ||
+			(words[temp[i]] == words[temp[j]] && temp[i] < temp[j])
+	})
+	if wordsLen > count {
+		temp = temp[:count]
+	}
+	return temp
 }
