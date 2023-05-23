04a92d58bf3a12cf27802666027a2b835bdbcfed
ildansaydiev <isaydiev86@gmail.com>
2023-05-23 11:24:46 +0500

    HW5 parallel execution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,65 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var (
+	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+	ErrNoWorkers           = errors.New("errors no worker")
+)
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if n <= 0 {
+		return ErrNoWorkers
+	}
+
+	guardCh := make(chan struct{}, n)
+	doneCh := make(chan bool, n)
+	var s, e int
+	var wg sync.WaitGroup
+
+	for _, task := range tasks {
+		guardCh <- struct{}{}
+
+		wg.Add(1)
+		go func(Task) {
+			defer wg.Done()
+			defer func() {
+				<-guardCh
+			}()
+
+			err := task()
+			doneCh <- err == nil
+		}(task)
+
+		err := checkResults(doneCh, &n, &m, &s, &e)
+		if err != nil {
+			return err
+		}
+	}
+
+	wg.Wait()
+	close(guardCh)
+	close(doneCh)
+
 	return nil
+}
+
+func checkResults(doneCh <-chan bool, n *int, m *int, s *int, e *int) (err error) {
+	done := <-doneCh
+
+	if done {
+		*s++
+	} else {
+		*e++
+		if *e >= *m || (*e+*s > *n+*m) {
+			err = ErrErrorsLimitExceeded
+		}
+	}
+
+	return err
 }
