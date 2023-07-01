0ede766b14ecc3263388297110a7278bcad96125
ildansaydiev <isaydiev86@gmail.com>
2023-07-01 08:10:00 +0500

    HW8 envdir tool

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"bufio"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+var ErrorInvalidFilename = errors.New("invalid filename")
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +25,63 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	fileInfos, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to read dir")
+	}
+
+	env := make(Environment, len(fileInfos))
+
+	for _, fInfo := range fileInfos {
+		if fInfo.IsDir() {
+			continue
+		}
+
+		if strings.Contains(fInfo.Name(), "=") {
+			return nil, ErrorInvalidFilename
+		}
+
+		var val string
+		val, err = getValueFromFile(path.Join(dir, fInfo.Name()))
+		if err != nil {
+			return nil, err
+		}
+
+		var info fs.FileInfo
+		info, err = fInfo.Info()
+		if err != nil {
+			return nil, err
+		}
+
+		env[fInfo.Name()] = EnvValue{
+			Value:      val,
+			NeedRemove: info.Size() == 0,
+		}
+	}
+
+	return env, nil
+}
+
+func getValueFromFile(fullName string) (string, error) {
+	f, err := os.Open(fullName)
+	if err != nil {
+		return "", errors.WithMessage(err, "failed to open file")
+	}
+	defer func() {
+		err = f.Close()
+		if err != nil {
+			log.Println("failed to close file: %w", err)
+		}
+	}()
+
+	buf := bufio.NewReader(f)
+	line, err := buf.ReadBytes('\n')
+	if err != nil && !errors.Is(err, io.EOF) {
+		return "", errors.WithMessage(err, "failed to read file")
+	}
+
+	val := strings.ReplaceAll(string(line), "\x00", "\n")
+	val = strings.TrimRight(val, " \t\n")
+
+	return val, nil
 }
